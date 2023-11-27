8f3cdd6bbd389b929dfaae929e931a43aceb1009
isaac <nodemadic@gmail.com>
2023-11-27 15:35:05 -0500

    Implements tag and ingredient filtering

--- app/user/tests/test_user_api.py
+++ app/user/tests/test_user_api.py
@@ -115,44 +115,45 @@ class PublicUserApiTests(TestCase):
 
         self.assertEqual(res.status_code, status.HTTP_401_UNAUTHORIZED)
 
-    class PrivateUserApiTests(TestCase):
-        """Test API requests that require authentication."""
 
-        def setUp(self):
-            self.user = create_user(
-                email='test@example.com',
-                password='testpass123',
-                name='Test Name',
-            )
-            self.client = APIClient()
-            self.client.force_authenticate(user=self.user)
+class PrivateUserApiTests(TestCase):
+    """Test API requests that require authentication."""
 
-        def test_retrieve_profile_success(self):
-            """Test retrieving profile for logged in user."""
-            res = self.client.get(ME_URL)
+    def setUp(self):
+        self.user = create_user(
+            email='test@example.com',
+            password='testpass123',
+            name='Test Name',
+        )
+        self.client = APIClient()
+        self.client.force_authenticate(user=self.user)
 
-            self.assertEqual(res.status_code, status.HTTP_200_OK)
-            self.assertEqual(res.data, {
-                'name': self.user.name,
-                'email': self.user.email,
-            })
+    def test_retrieve_profile_success(self):
+        """Test retrieving profile for logged in user."""
+        res = self.client.get(ME_URL)
 
-        def test_post_me_not_allowed(self):
-            """Test POST is not allowed for the me endpoint."""
-            res = self.client.post(ME_URL, {})
+        self.assertEqual(res.status_code, status.HTTP_200_OK)
+        self.assertEqual(res.data, {
+            'name': self.user.name,
+            'email': self.user.email,
+        })
 
-            self.assertEqual(
-                res.status_code,
-                status.HTTP_405_METHOD_NOT_ALLOWED
-            )
+    def test_post_me_not_allowed(self):
+        """Test POST is not allowed for the me endpoint."""
+        res = self.client.post(ME_URL, {})
 
-        def test_update_user_profile(self):
-            """Test updating the user profile for the authenticated user."""
-            payload = {'name': 'Updated name', 'password': 'newpassword123'}
+        self.assertEqual(
+            res.status_code,
+            status.HTTP_405_METHOD_NOT_ALLOWED
+        )
 
-            res = self.client.patch(ME_URL, payload)
+    def test_update_user_profile(self):
+        """Test updating the user profile for the authenticated user."""
+        payload = {'name': 'Updated name', 'password': 'newpassword123'}
 
-            self.user.refresh_from_db()
-            self.assertEqual(self.user.name, payload['name'])
-            self.assertTrue(self.user.check_password(payload['password']))
-            self.assertEqual(res.status_code, status.HTTP_200_OK)
+        res = self.client.patch(ME_URL, payload)
+
+        self.user.refresh_from_db()
+        self.assertEqual(self.user.name, payload['name'])
+        self.assertTrue(self.user.check_password(payload['password']))
+        self.assertEqual(res.status_code, status.HTTP_200_OK)
